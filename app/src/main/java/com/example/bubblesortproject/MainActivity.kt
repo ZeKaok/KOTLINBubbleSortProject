0f85eb11272ed7008006a222c0ffbd86a163dded
zehrahome <zehrakarakaya999@gmail.com>
2023-08-12 15:44:00 +0300

    textview'e append etme işlemi ve arryi aldırma işlemi tamamlandı.

--- app/src/main/java/com/example/bubblesortproject/MainActivity.kt
+++ app/src/main/java/com/example/bubblesortproject/MainActivity.kt
@@ -3,13 +3,16 @@ package com.example.bubblesortproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import com.example.bubblesortproject.databinding.ActivityMainBinding
+import org.w3c.dom.Text
 import java.lang.reflect.Array
 
 class MainActivity : AppCompatActivity() {
     var count=0
     private lateinit var binding: ActivityMainBinding
     var arr=IntArray(5)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityMainBinding.inflate(layoutInflater)
@@ -21,18 +24,20 @@ class MainActivity : AppCompatActivity() {
        arr.set(count,binding.editTextNumber.text.toString().toInt())
     }
     fun Goster(view: View){
+        var temp:Int=0
+        for(i in arr ){
+            for (j in arr){
+                if(arr[j]<arr[j+1]){
+                    temp=arr[j+1]
+                    arr[j+1]=arr[j]
+                    arr[j]=temp
+                }
+            }
+        }
+        for (i in arr){
+            binding.textView2.append(" "+arr[i])
+        }
 
-    }
-    fun Sort(){
-
-    }
-
-    fun Swap( num1:Int,num2: Int){
-
-        var tmp:Int=0
-        tmp=num1
-        num1=num2
-        num2=tmp
     }
 
 
