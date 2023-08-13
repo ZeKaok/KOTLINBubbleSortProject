37a77243815aea5726bf4093a3a2631c794d3d9a
zehrahome <zehrakarakaya999@gmail.com>
2023-08-13 11:30:14 +0300

    changed a bit

--- app/src/main/java/com/example/bubblesortproject/MainActivity.kt
+++ app/src/main/java/com/example/bubblesortproject/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.bubblesortproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Size
 import android.view.View
 import android.widget.TextView
 import com.example.bubblesortproject.databinding.ActivityMainBinding
@@ -9,7 +10,7 @@ import org.w3c.dom.Text
 import java.lang.reflect.Array
 
 class MainActivity : AppCompatActivity() {
-    var count:Int
+    var count:Int=0
     private lateinit var binding: ActivityMainBinding
     var arr= IntArray(5)
 
@@ -18,29 +19,34 @@ class MainActivity : AppCompatActivity() {
         binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.Al.setOnClickListener{
+            val size=arr.size
+            arr[count] = binding.editTextNumber.text.toString().toInt()
+            count++
+            bubbleSort(arr)
+        }
+        binding.Sirala.setOnClickListener {
+            binding.textView.clearComposingText()
+            for (i in arr){
+                binding.textView2.append(" "+arr[i])
+            }
+        }
     }
 
-    fun Al(view: View){
-        arr[count] = binding.editTextNumber.text.toString().toInt()
-        count++
-    }
-    fun Goster(view: View){
-        binding.textView.clearComposingText()
-        var temp:Int=0
-        val size=arr.size
-        for(i in 0 until  size-1){
-            for (j in 0 until  size-i-1){
-                if(arr[j]<arr[j+1]){
-                    temp=arr[j]
-                    arr[j]=arr[j+1]
-                    arr[j+1]=temp
+
+
+    private fun bubbleSort(arr: IntArray): IntArray {
+        val n = arr.size
+        for (i in 0 until n - 1) {
+            for (j in 0 until n - i - 1) {
+                if (arr[j] > arr[j + 1]) {
+                    val temp = arr[j]
+                    arr[j] = arr[j + 1]
+                    arr[j + 1] = temp
                 }
             }
         }
-        for (i in arr){
-            binding.textView2.append(" "+arr[i])
-        }
-
+        return arr
     }
 
 
